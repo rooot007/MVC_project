892807b8428782d518dd1c39bce28606303a03e1
Jun Tao <Jun@yahoo.com>
2016-11-24 00:56:22 +0200

    debug

--- components/Router.php
+++ components/Router.php
@@ -22,17 +22,8 @@ class Router
 		$uri = $this->getURI();
 		// Проверить наличие такого запроса в routes.php
 		foreach ($this->routes as $uriPattern => $path) {
-			// echo "<br>$uriPattern->$path";
 		//сравниваем строку запроса с данными из routes.php
 			if (preg_match("~$uriPattern~", $uri)) {
-				//--------------------------------------------------------------------------
-
-			// $uri = substr_replace($uri, '', 0, 20);
-			//------------------------------------------------------------------------------
-			// echo '<br>ищем запрос который набрал пользователь :' .$uri;
-			// echo '<br> что ищем по шаблону' . $uriPattern;
-			// echo '<br> кто обрабатывает'  . $path;
-
 			//получаем внутренний путь из внешнего согласно правилу.
 			$internalRoute = preg_replace("~$uriPattern~", $path, $uri);
 
